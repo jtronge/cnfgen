4a25807f636974e3c92ebcf6492814d26151052d
Maxim Zhulin <mzhulin3@gatech.edu>
2025-10-21 17:21:59 -0400

    Graph Coloring example

--- tests/test_graph_coloring.py
+++ tests/test_graph_coloring.py
@@ -1,4 +1,17 @@
-
+import cnfgen
 
 def test_graph_coloring():
-    pass
+    colors = ["red", "green", "blue"]
+    graph = [[0,0], [0,1], [0,2], [0,3], [1,2], [1,3], [2,3]]
+    nnodes = 4
+
+    cnf = cnfgen.ConstraintCompiler()
+    nodes = cnf.create_vars(nnodes, cnfgen.ENUM, values = colors)
+
+    for edge in graph:
+        vars = [nodes[edge[0]], nodes[edge[1]]]
+        cnf.add_constraint(vars, cnfgen.DIFFERENT)
+
+    cnf.output("test_graph_coloring.cnf")
+    result = cnf.solve()
+    assert result == "UNSATISFIABLE"
